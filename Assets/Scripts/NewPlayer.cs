5053d9481a86b2b195f26ac40312e6b413cf1d83
thales <thalestr@outlook.com>
2023-02-16 22:18:22 -0300

    feat: create a game layers

--- Assets/Scripts/NewPlayer.cs
+++ Assets/Scripts/NewPlayer.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewPlayer : MonoBehaviour
+public class NewPlayer : PhysicsObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+  [SerializeField] private float maxSpeed = 1;
+
+  // Start is called before the first frame update
+  void Start()
+  {
         
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    targetVelocity = new Vector2(Input.GetAxis("Horizontal")*maxSpeed, 0);
+
+    if(Input.GetButtonDown("Jump") && grounded)
+    {
+      velocity.y = 10;
     }
+  }
 }
