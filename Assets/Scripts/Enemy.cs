8b1ef8a775d93659d4ea00c48d900ae5f675c85d
thales <thalestr@outlook.com>
2023-02-25 14:29:22 -0300

    feat: add moves and attack to enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,19 @@ public class Enemy : PhysicsObject
 {
   [SerializeField] private float maxSpeed;
 
+  private int direction = 1;
   private RaycastHit2D leftLedgeRaycastHit;
   private RaycastHit2D rightLedgeRaycastHit;
 
+  private RaycastHit2D rightWallRaycastHit;
+  private RaycastHit2D leftWallRaycastHit;
+
+  [SerializeField] private LayerMask rayCastLayerMask;
+
   [SerializeField] private Vector2 rayCastOffset;
-  [SerializeField] private float raycastLength = 2;
+  [SerializeField] private float raycastLength = 1;
+
+  [SerializeField] private int attackPower = 10;
 
   void Start()
   {
@@ -20,12 +28,31 @@ public class Enemy : PhysicsObject
   // Update is called once per frame
   void Update()
   {
-    targetVelocity = new Vector2(maxSpeed, 0);
+    targetVelocity = new Vector2(maxSpeed*direction, 0);
 
     rightLedgeRaycastHit = Physics2D.Raycast(new Vector2(transform.position.x + rayCastOffset.x, transform.position.y + rayCastOffset.y), Vector2.down, raycastLength);
     Debug.DrawRay(new Vector2(transform.position.x + rayCastOffset.x, transform.position.y + rayCastOffset.y), Vector2.down * raycastLength, Color.yellow);
+    if(rightLedgeRaycastHit.collider == null) direction = -1;
 
     leftLedgeRaycastHit = Physics2D.Raycast(new Vector2(transform.position.x - rayCastOffset.x, transform.position.y + rayCastOffset.y), Vector2.down, raycastLength);
     Debug.DrawRay(new Vector2(transform.position.x - rayCastOffset.x, transform.position.y + rayCastOffset.y), Vector2.down * raycastLength, Color.yellow);
+    if (leftLedgeRaycastHit.collider == null) direction = 1;
+
+    rightWallRaycastHit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.right, raycastLength, rayCastLayerMask);
+    Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), Vector2.right * raycastLength, Color.blue);
+    if (rightWallRaycastHit.collider != null) direction = -1;
+
+    leftWallRaycastHit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.left, raycastLength, rayCastLayerMask);
+    Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), Vector2.left * raycastLength, Color.blue);
+    if (leftWallRaycastHit.collider != null) direction = 1;
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if(collision.gameObject == NewPlayer.Instance.gameObject)
+    {
+      NewPlayer.Instance.health -= attackPower;
+      NewPlayer.Instance.UpdateUI();
+    }
   }
 }
